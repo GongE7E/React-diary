b3a9908aedc790525d076f34f9d60d9eea27b4bb
gong <gong4737@icloud.com>
2023-10-05 21:19:13 +0900

    feat: Add notFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,18 @@
+import { useRouteError, useNavigate } from 'react-router-dom';
 import React from 'react';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const error = useRouteError();
+  console.log(error);
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Oops!!!!!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </div>
+  );
 }
