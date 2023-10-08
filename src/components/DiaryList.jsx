c5b9680f7752b09b72b340232d55a9cf68c07081
gong <gong4737@icloud.com>
2023-10-09 08:43:20 +0900

    feat: Add select options

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import './DiaryList.css';
 
+const sortOptionList = [
+  {
+    value: 'latest',
+    name: '최신순',
+  },
+  { value: 'oldest', name: '오래된순' },
+];
+
 export default function DiaryList() {
+  const [sortType, setSortType] = useState('latest');
+
   return (
     <div className='diaryList'>
       <div className='menu_wrapper'>
         <div className='left_col'>
-          <select></select>
+          <select
+            value={sortType}
+            onChange={(e) => setSortType(e.target.value)}
+          >
+            {sortOptionList.map((option) => (
+              <option key={option.value}>{option.name}</option>
+            ))}
+          </select>
         </div>
         <div className='right_col'>
           <Button type={'positive'} text={'새 일기쓰기'} />
