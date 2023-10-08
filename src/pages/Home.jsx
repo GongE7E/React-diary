c68b38382c91943ba33155a15616c51b942fc385
gong <gong4737@icloud.com>
2023-10-08 19:37:49 +0900

    feat: Add Home-Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
-import Editor from '../components/Editor';
+
 export default function Home() {
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년 ${
+    pivotDate.getMonth() + 1
+  }월`;
+
   return (
     <>
       <Header
-        title={'Home'}
-        leftChild={
-          <Button
-            text={'negative'}
-            onClick={() => alert('negative')}
-            type={'negative'}
-          />
-        }
-        rightChild={
-          <Button
-            text={'default'}
-            onClick={() => alert('default')}
-            type={'default'}
-          />
-        }
+        title={headerTitle}
+        leftChild={<Button text={'<'} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={onIncreaseMonth} />}
       />
-      <Editor onsubmit={() => alert('well done')} />
     </>
   );
+
+  function onIncreaseMonth() {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  }
+  function onDecreaseMonth() {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  }
 }
