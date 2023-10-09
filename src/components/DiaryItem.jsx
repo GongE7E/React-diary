4b09b3564377e75473da807bc7776aaf8f442996
gong <gong4737@icloud.com>
2023-10-09 21:10:28 +0900

    feat: Add diaryItem

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
+import './DiaryItem.css';
+import { getEmotionImgById } from '../utils/emotionFunction';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function DiaryItem({ item }) {
-  const { content } = item;
-  return <div>{content}</div>;
+  const { content, emotionId, date, id } = item;
+  const navigate = useNavigate();
+  return (
+    <div className='diaryItem'>
+      <div
+        className={['img_section', `img_section_${emotionId}`].join(' ')}
+        onClick={() => navigate(`/diary/${id}`)}
+      >
+        <img src={getEmotionImgById(emotionId)} alt={emotionId} />
+      </div>
+      <div className='info_section'>
+        <div className='data_wrapper'>
+          {new Date(parseInt(date)).toLocaleDateString()}
+        </div>
+        <div
+          className='content_wrapper'
+          onClick={() => navigate(`/diary/${id}`)}
+        >
+          {content}
+        </div>
+      </div>
+      <div className='button_section'>
+        <Button text={'수정하기'} onClick={() => navigate(`/edit/${id}`)} />
+      </div>
+    </div>
+  );
 }
