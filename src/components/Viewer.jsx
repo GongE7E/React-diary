1695f399c7c126103562df73db3b19f17ba6078e
gong <gong4737@icloud.com>
2023-10-10 17:11:14 +0900

    style : viewer

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -6,17 +6,22 @@ export default function Viewer({ content, emotionId }) {
   //   console.log(emotions);
   const emotionItem = emotions.find((element) => element.id === emotionId);
   return (
-    <div>
+    <div className='ly_viewer'>
       <section>
-        <h4>오늘의 감정</h4>
-        <div>
+        <h4 className='title'>오늘의 감정</h4>
+        <div
+          className={[
+            'emotion_img_wrapper',
+            `emotion_img_wrapper_${emotionId}`,
+          ].join(' ')}
+        >
           <img src={emotionItem.img} alt={emotionItem.name} />
+          <div className='emotion_descript'>{emotionItem.name}</div>
         </div>
-        <div>{emotionItem.name}</div>
       </section>
       <section>
-        <h4>오늘의 일기</h4>
-        <div>
+        <h4 className='title'>오늘의 일기</h4>
+        <div className='content_wrapper'>
           <p>{content}</p>
         </div>
       </section>
