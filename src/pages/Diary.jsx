efa82dd6092479ea3c671f8aa21b205a70f333ba
gong <gong4737@icloud.com>
2023-10-10 16:38:26 +0900

    feat:Add diary

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useDiary from '../hooks/useDiary';
+import getFormattedDate from '../utils/getFormattedDate';
+import Header from '../components/Header';
+import Button from '../components/Button';
 
 export default function Diary() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const data = useDiary(id);
   if (!data) {
     return <div>Loading...</div>;
   } else {
+    const { date } = data;
     return (
-      <div>
-        <h3>{id}</h3>
-      </div>
+      <>
+        <Header
+          title={`${getFormattedDate(new Date(Number(date)))}`}
+          leftChild={
+            <Button text={'< 뒤로가기'} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <Button
+              text={'수정하기 '}
+              onClick={() => navigate('/edit/${id}')}
+            />
+          }
+        />
+      </>
     );
   }
 }
