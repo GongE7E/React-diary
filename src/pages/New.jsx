9513663bc60c83f157cfd4afb2d0e0427c6a7e14
gong <gong4737@icloud.com>
2023-10-10 19:56:56 +0900

    feat: Add new page

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Editor from '../components/Editor';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import { DiaryDispatchContext } from '../App';
 
 export default function New() {
-  return <div>New</div>;
+  const navigate = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const handleSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate('/', { replace: true });
+  };
+  return (
+    <>
+      <Header
+        title={'새 일기 쓰기 '}
+        leftChild={<Button text={'< 뒤로가기'} onClick={() => navigate(-1)} />}
+      />
+      <Editor onsubmit={handleSubmit} />
+    </>
+  );
 }
