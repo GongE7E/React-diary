1e2f4f9b4afdc9e037b2ef4e604859ad78aaad7b
gong <gong4737@icloud.com>
2023-10-12 11:31:56 +0900

    refactor : diary  title & onClick

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -5,25 +5,28 @@ import getFormattedDate from '../utils/getFormattedDate';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Viewer from '../components/Viewer';
+
 export default function Diary() {
   const navigate = useNavigate();
   const { id } = useParams();
   const data = useDiary(id);
+
   if (!data) {
     return <div>Loading...</div>;
   } else {
     const { date, content, emotionId } = data;
+
     return (
       <>
         <Header
-          title={`${getFormattedDate(new Date(Number(date)))}`}
+          title={`${getFormattedDate(new Date(Number(date)))}기록`}
           leftChild={
             <Button text={'< 뒤로가기'} onClick={() => navigate(-1)} />
           }
           rightChild={
             <Button
               text={'수정하기 '}
-              onClick={() => navigate('/edit/${id}')}
+              onClick={() => navigate(`/edit/${id}`)}
             />
           }
         />
